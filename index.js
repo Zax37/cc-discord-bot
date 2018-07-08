b3aa959a6ef766e089599dfb05b99a2e5b9830e8
Zax37 <c37zax@gmail.com>
2018-07-08 13:48:19 +0200

    Quotes DB.

--- index.js
+++ index.js
@@ -10,21 +10,28 @@ const client = new Client({
 	connectionString: process.env.DATABASE_URL
 });
 
-// Initialize postgres database connection
+// Initialize postgres database connection and tables
 client.connect()
 
-client.query('SELECT $1::text as message', ['Hello world!'], (err,res) => {
+var quotes = [];
+client.query('CREATE TABLE IF NOT EXISTS QUOTES (id varchar(20), text text);')
+  .then(_ => 
+	client.query('SELECT * FROM QUOTES;')
+	.then(res => console.log(res.rows))
+	.catch(e => console.error(e.stack))
+  ).catch(e => console.error(e.stack));
+
+/*client.query('SELECT $1::text as message', ['Hello world!'], (err,res) => {
 	console.log(res.rows[0].message) // Hello world!
 	client.end()
-});
+});*/
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: process.env.BOT_TOKEN,
    autorun: true
 });
-var quotes = [];	// zax               // szysza           //465276634849738753 - tnt bot
-var admins = [ 185756278335864833, 332963324616769550 ];
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
@@ -32,8 +39,6 @@ bot.on('ready', function (evt) {
 	bot.setPresence({ game: { name: 'Claw', type: 0 } });
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '/') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
@@ -41,11 +46,18 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         args = args.splice(1);
         switch(cmd) {
             case 'quote':
-				console.log(args);
 				if (args[0] == 'add') {
 					var id = args[1];
 					args = args.splice(2);
 					quotes[id] = args.join(" ");
+					client.query('INSERT INTO QUOTES VALUES($1::text, $2::text);', [id, quotes[id]])
+						.catch(e => {
+							bot.sendMessage({
+								to: channelID,
+								message: "Sorry, I've had some problem inserting this quote :("
+							});
+							console.error(e.stack);
+						});
 				} else {
 					var id = args[0];
 					args = args.splice(1);
@@ -63,7 +75,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					});
 				}
             break;
-            // Just add any case commands if you want to..
          }
      }
 });
