4c2d2952f229314bce9b95fda3f680d3f82d5152
Zax37 <c37zax@gmail.com>
2018-07-08 14:40:44 +0200

    Fixed displaying quotes.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const express = require('express');
 const app = express();
 
 app.set('json spaces', 4);
-app.get('/', (req, res) => res.json(quotes));
+app.get('/', (req, res) => res.json({quotes}));
 app.listen(process.env.PORT || 5000, () => console.log('Listening on port '+(process.env.PORT || 5000)));
 
 // Initialize Discord Bot
